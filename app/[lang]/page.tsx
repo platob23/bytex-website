1e01d128d4d4d80a1fcb545b3c94ce10f2dddcd7
Tobias Plank <platob23@htl-kaindorf.at>
2026-04-07 19:53:16 +0200

    feat(page): wire sections and add translations

---
+++ app/[lang]/page.tsx
@@ -0,0 +1,27 @@
+import { notFound } from 'next/navigation'
+import { getDictionary, hasLocale } from './dictionaries'
+import Navbar from '../../components/Navbar'
+import Hero from '../../components/Hero'
+import Services from '../../components/Services'
+import References from '../../components/References'
+import Process from '../../components/Process'
+
+export default async function Page({ params }: PageProps<'/[lang]'>) {
+  const { lang } = await params
+
+  if (!hasLocale(lang)) notFound()
+
+  const dict = await getDictionary(lang)
+
+  return (
+    <>
+      <Navbar lang={lang} nav={dict.nav} />
+      <main>
+        <Hero hero={dict.hero} />
+        <Services services={dict.services} />
+        <References references={dict.references} />
+        <Process process={dict.process} />
+      </main>
+    </>
+  )
+}
